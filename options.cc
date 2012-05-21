9734ed5eabec7b4d116652d66c09c2e81543fd7f
lacozhang <lacozhang@gmail.com>
2012-05-21 15:48:29 +0100

    options ready, we can move to next state

--- options.cc
+++ options.cc
@@ -1,4 +1,5 @@
 #include "options.h"
+#include<iostream>
 #include<boost/regex.hpp>
 
 options::options( std::string raw ){
@@ -8,18 +9,40 @@ options::options( std::string raw ){
 options::~options(){
 }
 
-options::iterator options::begin(){
+options::iterator options::begin() const{
   return m_body.begin();
 }
 
-options::iterator options::end(){
+options::iterator options::end() const{
   return m_body.end();
 }
 
-std::string options::head(){
+std::string options::head() const{
   return m_head;
 }
 
+//file format : head:options1\toptions2\t...optionsn\n
 void options::split( std::string raw ){
 
+  boost::regex hs(":");//split head and body
+  boost::regex bs("\t+");//split body
+
+  boost::sregex_token_iterator hs_it(raw.begin(),
+									  raw.end(),
+									  hs, -1);
+  boost::sregex_token_iterator end;
+
+  std::string head, body;
+  m_head = hs_it->str();
+  hs_it++;
+  body = hs_it->str();
+
+  boost::sregex_token_iterator bs_it( body.begin(),
+									  body.end(),
+									  bs, -1);
+  while( bs_it != end ){
+	m_body.push_back( *bs_it);
+	++bs_it;
+  }
+ 
 }
